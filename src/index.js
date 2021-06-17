4f44675f596896d7ca312e93128cadd2c1fa65b9
dmitry <pisarev_dmitry@mail.ru>
2021-06-17 07:26:53 +0300

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import parseFile from './parsers.js';
 import compare from './compare.js';
 import format from './formatters/index.js';
 
-const gendiff = (filepath1, filepath2, outputFormat) => {
+const gendiff = (filepath1, filepath2, outputFormat = 'stylish') => {
   const pathToFile1 = path.resolve(process.cwd(), filepath1);
   const pathToFile2 = path.resolve(process.cwd(), filepath2);
   const file1 = readFileSync(pathToFile1, 'utf-8');
@@ -13,6 +13,7 @@ const gendiff = (filepath1, filepath2, outputFormat) => {
   const data1 = parseFile(file1, path.extname(pathToFile1).slice(1));
   const data2 = parseFile(file2, path.extname(pathToFile2).slice(1));
   const compared = compare(data1, data2);
+  console.log(outputFormat, compared);
   const result = format(compared, outputFormat);
   return result;
 };
