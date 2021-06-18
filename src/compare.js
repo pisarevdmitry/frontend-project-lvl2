78ba1d45bd93e9a9e442ffaef6d64f8f27b659b6
dmitry <pisarev_dmitry@mail.ru>
2021-06-18 17:39:14 +0300

    little ref

--- src/compare.js
+++ src/compare.js
@@ -2,42 +2,28 @@ import _ from 'lodash';
 
 const cloneValue = (value) => (_.isObject(value) ? _.cloneDeep(value) : value);
 
+const createElem = (status, value, oldValue = null) => ({
+  status,
+  value,
+  oldValue,
+});
+
 const compare = (obj1, obj2) => {
   const mergedKeys = Object.keys({ ...obj1, ...obj2 });
   const result = mergedKeys.reduce((acc, key) => {
     if (!_.has(obj1, key)) {
-      const elem = {
-        status: 'added',
-        value: cloneValue(obj2[key]),
-      };
-      return { ...acc, [key]: elem };
+      return { ...acc, [key]: createElem('added', cloneValue(obj2[key])) };
     }
     if (!_.has(obj2, key)) {
-      const elem = {
-        status: 'deleted',
-        value: cloneValue(obj1[key]),
-      };
-      return { ...acc, [key]: elem };
+      return { ...acc, [key]: createElem('deleted', cloneValue(obj1[key])) };
     }
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      const elem = {
-        value: compare(obj1[key], obj2[key]),
-      };
-      return { ...acc, [key]: elem };
+      return { ...acc, [key]: createElem(null, compare(obj1[key], obj2[key])) };
     }
     if (obj1[key] !== obj2[key]) {
-      const elem = {
-        status: 'changed',
-        oldValue: obj1[key],
-        value: obj2[key],
-      };
-      return { ...acc, [key]: elem };
+      return { ...acc, [key]: createElem('changed', obj2[key], obj1[key]) };
     }
-    const elem = {
-      status: 'unchanged',
-      value: obj1[key],
-    };
-    return { ...acc, [key]: elem };
+    return { ...acc, [key]: createElem('unchanged', obj1[key]) };
   }, {});
   return result;
 };
